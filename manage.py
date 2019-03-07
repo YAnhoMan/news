bb8d6878872bfd6a8257cebdf922f348ca655893
YAnhoMan <wyanho@foxmail.com>
2019-03-07 15:18:35 +0800

    firsttimes

--- manage.py
+++ manage.py
@@ -2,31 +2,39 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from redis import StrictRedis
 from flask_wtf import CSRFProtect
-import pymysql
 from flask_session import Session
+import pymysql
 pymysql.install_as_MySQLdb()
 
 class Config(object):
     # 开启debug模式
     DEBUG = True
 
-    # 设置session密钥
-    SECRET_KEY = "EjpNVSNQTyGi1VvWECj9TvC/+kq3oujee2kTfQUs8yCM6xX9Yjq52v54g+HVoknA"
-    SESSION_TYPE = "redis" # 指定 session 保存到 redis 中
-    SESSION_USE_SIGNER = True # 让 cookie 中的 session_id 被加密签名处理
-    PERMANENT_SESSION_LIFETIME = 86400 # session 的有效期，单位是秒
-
-
-    # mysql数据库相关配置
-    # 连接数据库配置
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:123456@127.0.0.1:3306/news'
-    # 关闭数据库修改跟踪
-    SQLAlCHEMY_TRACK_MODIFICATIONS = False
-
     # 初始化redis配置
     REDIS_HOST ='127.0.0.1'
     REDIS_PORT = 6379
 
+    # 设置session密钥
+    SECRET_KEY = "EjpNVSNQTyGi1VvWECj9TvC/+kq3oujee2kTfQUs8yCM6xX9Yjq52v54g+HVoknA"
+
+    # 指定 session 保存到 redis 中
+    SESSION_TYPE = "redis"
+
+    # 具体保存到哪个数据库
+    SESSION_REDIS = StrictRedis(host=REDIS_HOST, port=REDIS_PORT, db=1)
+
+    # 让 cookie 中的 session_id 被加密签名处理
+    SESSION_USE_SIGNER = True
+
+    # session 的有效期，单位是秒
+    PERMANENT_SESSION_LIFETIME = 86400
+
+    # mysql数据库相关配置
+    # 连接数据库配置
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:123456@127.0.0.1:3306/news'
+
+    # 关闭数据库修改跟踪
+    SQLAlCHEMY_TRACK_MODIFICATIONS = False
 
 
 # 初始化APP对象
@@ -36,6 +44,10 @@ app.config.from_object(Config)
 
 # 设置CSRF保护
 CSRFProtect(app)
+
+# 设置session保存在redis里面
+Session(app)
+
 
 # 创建mysql数据库对象
 db = SQLAlchemy(app)
