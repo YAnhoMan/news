691b2350c9ba09c28e374601e6362d184605132c
YAnhoMan <wyanho@foxmail.com>
2019-03-10 21:54:12 +0800

    首页列表完成了!!!

--- info/moduls/index/views.py
+++ info/moduls/index/views.py
@@ -1,8 +1,74 @@
 from info.models import User, News, Category
 from ..index import index_bp
-from flask import render_template, current_app, session, jsonify
+from flask import render_template, current_app, session, jsonify, request
 from info import redis_store, constants
 from info.response_code import RET
+
+
+@index_bp.route('/news_list', methods=['GET'])
+def get_news_list():
+    """获取首页新闻列表数据接口"""
+
+    """
+    1.获取参数
+        1.1 cid: 当前分类id，page:当前页码，默认值：1， per_page:每一页多少条数据，默认值：10
+    2.参数校验
+        2.1 判断cid是否为空
+        2.2 将数据进行int强制类型转换
+    3.逻辑处理
+        3.1 根据cid作为查询条件获取查询对象，再调用paginate方法进行数据分页处理
+        3.2 调用分类对象的属性 获取当前页所有数据，当前页码，总页码
+        3.3 将新闻对象列表转换成字典列表
+    4.返回值
+    """
+    cid = request.args.get('cid')
+
+    page = request.args.get('page', 1)
+
+    per_page = request.args.get('per_page', 10)
+
+    if not cid:
+        return jsonify(errno=RET.PARAMERR, errmsg="参数不足")
+
+    try:
+        cid = int(cid)
+        page = int(page)
+        per_page = int(per_page)
+
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.PARAMERR, errmsg="参数格式错误")
+
+    filter_list = []
+
+    if cid != 1:
+        filter_list.append(News.category_id == cid)
+
+    try:
+        paginate = News.query.filter(*filter_list).order_by(News.create_time.desc()).paginate(page, per_page, False)
+
+        news_list = paginate.items
+
+        current_page = paginate.page
+
+        total_page = paginate.pages
+
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR, errmsg="查询数据库出错")
+
+    news_dict_list = []
+
+    for news in news_list:
+        news_dict_list.append(news.to_dict())
+
+    data = {
+        'news_dict_list': news_dict_list,
+        'current_page': current_page,
+        'total_page': total_page
+    }
+
+    return jsonify(errno=RET.OK, errmsg='OK', data=data)
 
 
 @index_bp.route('/')
--- info/static/news/js/index.js
+++ info/static/news/js/index.js
@@ -1,10 +1,16 @@
 var currentCid = 1; // 当前分类 id
 var cur_page = 1; // 当前页
 var total_page = 1;  // 总页数
-var data_querying = true;   // 是否正在向后台获取数据
+// 定义标志位
+var data_querying = false;   // 是否正在向后台获取数据 false没有往后台请求数据
 
 
 $(function () {
+
+    // 注意： 请求新闻列表数据
+    updateNewsData()
+
+
     // 首页分类切换
     $('.menu li').click(function () {
         var clickCid = $(this).attr('data-cid')
@@ -13,6 +19,7 @@ $(function () {
         })
         $(this).addClass('active')
 
+        // 表示点击别的分类了
         if (clickCid != currentCid) {
             // 记录当前分类id
             currentCid = clickCid
@@ -41,10 +48,71 @@ $(function () {
 
         if ((canScrollHeight - nowScroll) < 100) {
             // TODO 判断页数，去更新新闻数据
+            // console.log("来了，老弟！！")
+            // 会请求多次，限定请求次数
+            // data_querying=false 没有人往后端请求数据，我可以向后端请求数据
+            if(!data_querying){
+                // 当前页码小于等于总页数
+               if(cur_page <= total_page){
+                    // 往后端请求数据
+                    data_querying = true
+                    updateNewsData()
+               }else{
+                   // 页码超标
+                   data_querying = false
+               }
+
+            }
+
+
         }
     })
 })
 
+// 请求首页新闻列表数据
 function updateNewsData() {
-    // TODO 更新新闻数据
+
+    var params = {
+        "page": cur_page,
+        "cid": currentCid,
+    }
+    $.get("/news_list",
+        params,
+        function (resp) {
+        if (resp) {
+
+            // 只有第一次请求的时候才需要清空数据
+            if (cur_page == 1){
+                // 先清空原有数据
+                $(".list_con").html('')
+
+            }
+
+            // 总页数赋值
+            total_page = resp.data.total_page
+
+            // 请求下一页数据 页码累加
+            cur_page += 1
+
+            // 当数据加载完毕，需要将data_querying设置为false，方便下次请求下一页数据
+            data_querying = false
+
+            // 显示数据
+            for (var i=0;i<resp.data.news_dict_list.length;i++) {
+                var news = resp.data.news_dict_list[i]
+                var content = '<li>'
+                content += '<a href="#" class="news_pic fl"><img src="' + news.index_image_url + '?imageView2/1/w/170/h/170"></a>'
+                content += '<a href="#" class="news_title fl">' + news.title + '</a>'
+                content += '<a href="#" class="news_detail fl">' + news.digest + '</a>'
+                content += '<div class="author_info fl">'
+                content += '<div class="source fl">来源：' + news.source + '</div>'
+                content += '<div class="time fl">' + news.create_time + '</div>'
+                content += '</div>'
+                content += '</li>'
+
+                // 添加新闻数据到ul标签中
+                $(".list_con").append(content)
+            }
+        }
+    })
 }
